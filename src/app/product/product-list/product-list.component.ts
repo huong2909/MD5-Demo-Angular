c1a8a31ae772cdb2700518d1f3b4486c2d45ace8
huong2909 <huong>
2022-07-05 09:16:45 +0700

    thuchanh

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../service/product.service';
+import {Product} from '../../model/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+  constructor(public productService: ProductService) { }
+
+  ngOnInit() {
+    this.getAll();
+  }
+  getAll() {
+    this.products = this.productService.getAll();
+  }
+}
