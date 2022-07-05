74f263a5d9606e032f31fe1da8c97a6b346d2265
huong2909 <huong>
2022-07-05 16:53:13 +0700

    crud ok

--- src/app/product/product-delete/product-delete.component.ts
+++ src/app/product/product-delete/product-delete.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {ProductService} from '../../service/product.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-product-edit',
-  templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.scss']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.scss']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
+
   productForm: FormGroup;
   id: number;
 
   constructor(private productService: ProductService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.id = +paramMap.get('id');
       const product = this.getProduct(this.id);
@@ -25,16 +27,17 @@ export class ProductEditComponent implements OnInit {
       });
     });
   }
+
+  ngOnInit() {
+  }
+
   getProduct(id: number) {
     return this.productService.findById(id);
   }
-  updateProduct(id: number) {
-    const product = this.productForm.value;
-    this.productService.updateProduct(id, product);
-    alert('Cập nhật thành công');
-  }
 
-  ngOnInit() {
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id);
+    this.router.navigate(['/product/list']);
   }
 
 }
